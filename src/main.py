dd76f495d5ca1178ee85ccf036b0c97dfd8d54ab
WarpWing <professornitro@gmail.com>
2021-04-09 19:49:22 -0400

    Pain. Suffering. Success though ^^ Basic Form is going well

--- src/main.py
+++ src/main.py
@@ -11,32 +11,47 @@ from discord.ext import commands
 from dotenv import load_dotenv
 
 
-#Startup functions
-load_dotenv()
+# Startup process
+load_dotenv() # Loads Enviromental Secretsanswers = []
 # Variables 
 TOKEN = os.getenv('DISCORD_TOKEN')
 FILEPATH = os.getcwd()
-#Load Config File
+# Load Config File
 with open(f'{FILEPATH}/src/config.yml') as file: 
     config = yaml.full_load(file)
- 
+
 
 # Bot Related Code 
-bot = commands.Bot(command_prefix='!')
-bot.remove_command("help")
+bot = commands.Bot(command_prefix='!') 
 
 @bot.event  # Sends message to terminal once bot is ready
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
     print(f'{bot.user} has loaded up Modules!')
 
-    activity = discord.Game(name="Oversight v0.1", type=3)
-    await bot.change_presence(status=discord.Status.online, activity=activity)
+    game = discord.Game("Oversight v0.1")
+    await bot.change_presence(status=discord.Status.online, activity=game)
 
-# Register command 
+
+# Register command(Will be removed)
 @bot.command()
-async def form(ctx):
-    await ctx.send(config)
+async def apply(ctx):
+    answers = []
+    user = ctx.author
+    for idx, question in enumerate(config["questions"]):
+        await dm(user, msg=f"Question #{idx + 1}: {question}")
+        response = await bot.wait_for('message')
+        answers.append(response.content)
+
+    
+# @client.event
+# asnyc def on_message(message):
+#     return await client.wait_for('message', check=check)
+
+
+async def dm(user, msg=None):
+    msg = msg or "Error, message was empty."
+    await user.send(msg)
 
 bot.run(f'{TOKEN}')
 
