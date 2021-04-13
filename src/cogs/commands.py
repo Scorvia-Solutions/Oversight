23a364ff2760d3868b3d63abbebd5c5ea8a9c506
Dylan R <d.raiff@icloud.com>
2021-04-12 20:25:03 -0400

    decompartmentalized code

--- src/cogs/commands.py
+++ src/cogs/commands.py
@@ -2,6 +2,7 @@
 import os, json, discord, yaml, dotenv
 
 from discord.ext import commands
+from discord.utils import get
 from utils import default
 from utils.objects import Embed
 #endregion
@@ -11,6 +12,9 @@ class Commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+        dotenv.load_dotenv()
+        self.application_channel = os.getenv('APPLICATION_CHANNEL')
+
         with open(f'{os.getcwd()}/src/content.yml') as f:
             self.content = yaml.full_load(f)
 
@@ -18,38 +22,34 @@ class Commands(commands.Cog):
     @commands.command(name='apply')
     async def apply(self, ctx):
         satisfied = False
-        while not satisfied:
-            response, answers = await self.ask_questions(ctx)
-            satisfied = response
 
-        await ctx.author.message('Your application is being processed, please be patient.')
+        """ Asks the user the questions in content.yml through thier direct messages, and allows them to reanswer until satisfaction """
+        while not satisfied:
+            answers = []
+
+            for idx, question in enumerate(self.content['questions']):
+                await ctx.author.send(f'__#{idx + 1}: {question}__')
+                response = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author and not m.guild)
+                answers.append(response.content)
+
+            embed = Embed(bot=self.bot, title='Oversight', color=0xFF5733)
+            for (idx, question), answer in zip(enumerate(self.content['questions']), answers): 
+                embed.add_field(name=f'#{idx + 1}: {question}', value=f'*{answer}*', inline=False)
+            
+            await ctx.author.send(embed=embed)   
+            await ctx.author.send(f'Are these answers correct? **(y/n)**')
+
+            satisfied = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author and not m.guild and (m.content == 'y' or m.content =='n'))
+
+        await ctx.author.send('Your application is being processed, please be patient.')
         await self.send_to_applications(ctx.author, answers)
 
-    async def send_to_applications(self, user, answers):
-        pass
-        
-    """ Asks the user the questions in content.yml through thier direct messages, returns if they like their answers, and an array containing their answers. """
-    async def ask_questions(self, ctx):
-        answers = []
+        """ Sends the user's application to the application channel. """
+        # Reuse previous embed
+        embed.title = f'{ctx.author}\'s Application'
+        application_channel = get(ctx.guild.channels, name=self.application_channel)
+        await application_channel.send(embed=embed)
 
-        for idx, question in enumerate(self.content['questions']):
-            await self.dm(ctx.author, msg=f'__#{idx + 1}: {question}__')
-            response = await self.bot.wait_for('message', check=lambda m: m.author == ctx.author and not m.guild)
-            answers.append(response.content)
-
-        embed=Embed(bot=self.bot, title='Oversight', color=0xFF5733)
-        for (idx, question), answer in zip(enumerate(self.content['questions']), answers): 
-            embed.add_field(name=f'#{idx + 1}: {question}', value=f'*{answer}*', inline=False)
-        
-        await ctx.author.send(embed=embed)   
-        await ctx.author.send(f'Are these answers correct? **(y/n)**')
-
-        return (await self.bot.wait_for('message', check=lambda m: m.author == ctx.author and not m.guild and (m.content == 'y' or m.content =='n')), answers)
-
-    """ Sends the message as a direct message to the user. """
-    async def dm(self, user, msg=None):
-        msg = msg or 'Error, message was empty.'
-        await user.send(msg) 
 
 def setup(bot):
     bot.add_cog(Commands(bot))
